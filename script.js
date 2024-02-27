afea8a9289c056e47bf438281995f36b023b9c77
bandreid01 <viniciusdasilvax@gmail.com>
2024-02-27 16:42:13 -0300

    HTML e CSS Básico

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const exercicios = [
         titulo: "m1s04",
         url: "M1S04/index.html"
     },
+    {
+        titulo: "m1s06",
+        url: "M1S06/index.html"
+    }
 ]
 
 const listaExercicios = document.querySelector("#lista-exercicios")
@@ -13,3 +17,4 @@ exercicios.map((exercicio)=>{
         <h2><a href="${exercicio.url}">${exercicio.titulo}</a></h2>
     </div>`
 })
+
