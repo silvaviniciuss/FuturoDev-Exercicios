85580553d0a7934b4e40d4c44d0cca3e671199ed
bandreid01 <viniciusdasilvax@gmail.com>
2024-03-05 15:16:07 -0300

    Página de Login Finalizada

--- M1S07/script.js
+++ M1S07/script.js
@@ -1,6 +1,6 @@
 const listaExercicios = document.querySelector("#lista-exercicios")
 
-for (let i = 1; i <= 1; i++) {
+for (let i = 1; i <= 10; i++) {
     listaExercicios.innerHTML +=
     `<div id="exercicios">
         <h2><a href="Exercicio ${i}/index.html">Exercicío ${i}</a></h2>
